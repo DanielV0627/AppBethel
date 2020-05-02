91e8a0ea2513c76866015b52870acec636e23e92
Josue <j5p4b7k@gmail.com>
2020-05-02 15:58:45 -0500

    Agregamos los componentes company y product

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CompanyComponent } from './component/company/company.component';
+import { ProductComponent } from './component/product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompanyComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
